7f879e034da0dffb1a08e89e37b810de5162226a
noLimits97 <ks971119@naver.com>
2022-10-28 04:53:57 +0900

    router-basic-setup

--- src/server.js
+++ src/server.js
@@ -1,27 +1,28 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
 const PORT = 4000;
+const logger = morgan("dev");
 
-const middleWare = (req, res, next) => {
+const middleware = (req, res, next) => {
   if (req.protocol != "https") {
     console.log("Insecure");
   }
   console.log("Someone is trying to enter this url. ⛔");
-  console.log(`Method: ${req.path}`);
   next();
 };
-const homeRes = (req, res) => {
+const handleHome = (req, res) => {
   console.log("** This is the response for home. **");
   return res.send("<h1>Response for home Completed</h1>");
 };
-const loginRes = (req, res) => {
+const handleLogin = (req, res) => {
   console.log("** This is the response for login. **");
   return res.send("<h1>Response for login Completed</h1>");
 };
-app.use(middleWare);
-app.get("/", homeRes);
-app.get("/login", loginRes);
+app.use(middleware, logger);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on http://localhost:${PORT}🌏`);
